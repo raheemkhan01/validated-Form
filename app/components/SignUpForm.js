420f127cd22f14493d08865e2b654a105b09d8ef
raheemforofficial <raheemforofficial@gmail.com>
2024-04-17 12:26:03 +0530

    Updated

--- app/components/SignUpForm.js
+++ app/components/SignUpForm.js
@@ -1,9 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import React, { useEffect, useState } from "react";
-import Validation from "./Validation";
+import { z } from "zod";
+import SignUpDone from "./SignUpDone";
 
+const userSchema = z.object({
+  fullname: z.string().min(3).max(20),
+  email: z.string().email(),
+  password: z.string().min(8).max(20),
+});
 function SignUpForm({ SubmitButton }) {
-  const [value, setValue] = useState({
+  const [formData, setFormData] = useState({
     fullname: "",
     email: "",
     password: "",
@@ -11,25 +18,40 @@ function SignUpForm({ SubmitButton }) {
 
   const [errors, setErrors] = useState({});
 
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    if (Object.keys(errors).length === 0 && data) {
-      SubmitButton(true);
-    }
-  }, [errors]);
-
-  const handleOnClick = (e) => {
-    e.preventDefault();
-    setErrors(Validation(value));
-    setData(true);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const parsedValue = name === "age" ? parseInt(value, 10) : value;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: parsedValue,
+    }));
   };
 
-  const handleOnChange = (e) => {
-    setValue({
-      ...value,
-      [e.target.name]: e.target.value,
-    });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      const validatedData = userSchema.parse(formData);
+
+      console.log("Form data is valid:", validatedData);
+      setErrors({});
+      if(validatedData.errors.length > 0) {
+        SubmitButton(true);
+      }
+    } catch (error) {
+      console.error("Form data validation error:", error);
+      const customErrors = {};
+      error.errors.forEach((err) => {
+        if (err.path[0] === "fullname") {
+          customErrors.fullname = "  Fullname is required";
+        } else if (err.path[0] === "email") {
+          customErrors.email = "  Email is required";
+        } else if (err.path[0] === "password") {
+          customErrors.password = " Password is required ";
+        } 
+      });
+      setErrors(customErrors);
+    }
   };
 
   return (
@@ -38,47 +60,44 @@ function SignUpForm({ SubmitButton }) {
         <div className="shape"></div>
         <div className="shape"></div>
       </div>
-      <form autoComplete="off" className="form">
+      <form autoComplete="off" className="form" onSubmit={handleSubmit}>
         <h3 className="h3">Sign Up Here</h3>
-        <label htmlFor="username" className="label">
-          Full Name
-        </label>
+        <label className="label">Full Name</label>
         <input
-          value={value.fullname}
-          onChange={handleOnChange}
           type="text"
           className="input"
           placeholder="Enter Your Name"
           name="fullname"
+          value={formData.fullname}
+          onChange={handleChange}
         />
         {errors.fullname && <p className="p">{errors.fullname}</p>}
         <label htmlFor="username" className="label">
-          Full Name
+          Email
         </label>
         <input
-          value={value.email}
-          onChange={handleOnChange}
           type="email"
           className="input"
           placeholder="Enter Your Email"
           name="email"
+          value={formData.email}
+          onChange={handleChange}
         />
         {errors.email && <p className="p">{errors.email}</p>}
-        <label htmlFor="username" className="label">
-          Full Name
+        <label htmlFor="password" className="label">
+          Password
         </label>
         <input
-          value={value.password}
-          onChange={handleOnChange}
           type="password"
           placeholder="Enter Your Password"
+          id="password"
           name="password"
           className="input"
+          value={formData.password}
+          onChange={handleChange}
         />
         {errors.password && <p className="p">{errors.password}</p>}
-        <button className="button" onClick={handleOnClick}>
-          Sumbit
-        </button>
+        <button className="button">Submit</button>
       </form>
     </>
   );
